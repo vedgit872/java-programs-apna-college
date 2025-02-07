80e93fc0df9db5fd3a91c64097df2284b9564d06
vedgit872 <vedsmith18@gmail.com>
2025-02-07 19:46:52 +0530

    bfs and dfs

--- imp/graph/start.java
+++ imp/graph/start.java
@@ -1,5 +1,9 @@
 package imp.graph;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.management.Query;
 
 @SuppressWarnings("unchecked")
 public class start {
@@ -12,6 +16,46 @@ public class start {
             this.dest=d;
             this.wt=w;
         }
+    }
+    public static void bfs(ArrayList<edge>[] graph){
+              Queue<Integer> q=new LinkedList<>();
+              boolean vis[]=new boolean[graph.length];
+              q.add(0);
+              while (!q.isEmpty()) {
+                int curr=q.remove();
+                if (!vis[curr]) {
+                    System.out.println(curr+" ");
+                    vis[curr]=true;
+                    for (int i = 0; i < graph[curr].size(); i++) {
+                        q.add(graph[curr].get(i).dest);
+                    }
+                }
+              }
+    }
+    public static void dfs(ArrayList<edge>[] graph,int curr,boolean vis[]){
+        System.out.println(curr+" ");
+        vis[curr]=true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            edge e =graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfs(graph, e.dest, vis);
+            }
+
+        }
+    }
+    public static boolean haspath(ArrayList<edge>[] graph,int curr,int dest,boolean vis[]){
+        if (curr==dest) {
+            return true;
+        }
+        vis[curr]=true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            edge e =graph[curr].get(i);
+            if (!vis[e.dest] && haspath(graph, e.dest, dest, vis)) {
+                return true;
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
         int v=5;
@@ -35,8 +79,13 @@ public class start {
         graph[4].add(new edge(4,2,2));
 
         //neighbours of 2
-        for (int i = 0; i < graph[2].size(); i++) {
-            System.out.println(graph[2].get(i).dest);
-        }
+        // for (int i = 0; i < graph[2].size(); i++) {
+        //     System.out.println(graph[2].get(i).dest);
+        // }
+
+        // bfs(graph);
+        boolean vis[]=new boolean[graph.length];
+        // dfs(graph,0,vis);
+        System.out.println(haspath(graph, 0, 5, vis));
     }
 }
