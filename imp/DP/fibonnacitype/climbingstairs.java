d8ce6995eb11980970d69ce61cd370b36ea4a0c0
vedgit872 <vedsmith18@gmail.com>
2025-02-20 18:24:52 +0530

    DP basics

---
+++ imp/DP/fibonnacitype/climbingstairs.java
@@ -0,0 +1,41 @@
+package imp.DP.fibonnacitype;
+
+public class climbingstairs {
+    public static int recStairs(int n){
+        if (n==2||n==1) {
+            return n;
+        }
+        return recStairs(n-1)+recStairs(n-2);
+    }
+    public static int recStairs(int n,int dp[]){
+        //memoization
+        if (n==2||n==1) {
+            return n;
+        }
+        if (dp[n]!=0) {
+            return dp[n];
+        }
+        dp[n]=recStairs(n-1,dp)+recStairs(n-2,dp);
+        return dp[n];
+    }
+    public static int recStair(int n){
+        //tabulation
+        if (n == 1) return 1;
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for (int i = 3; i <=n; i++) {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println("Recursion: " + recStairs(n));
+        
+        int dp[] = new int[n + 1];
+        System.out.println("Memoization: " + recStairs(n, dp));
+        
+        System.out.println("Tabulation: " + recStair(n));
+    }
+}
