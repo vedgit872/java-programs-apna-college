a952ee2d1f4a01a29d27de76b4d5240bec85d896
vedgit872 <vedsmith18@gmail.com>
2025-05-30 17:13:45 +0530

    Day 31-Binary Tree and Array
    In Flipping of image we swap numbers  by column and invert them simultaneously. In Finding maximum depth we use recursion in which we calculate left subtree depth and right subtree depth and return the max one.

---
+++ leetcode/BinaryTree/MaximumDepthOfABinaryTree.java
@@ -0,0 +1,65 @@
+package leetcode.BinaryTree;
+
+/*Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100 */
+
+public class MaximumDepthOfABinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.right == null && root.left == null) {
+            return 1;
+        }
+        int r = 0, l = 0;
+        if (root.right != null) {
+            r = maxDepth(root.right);
+        }
+        if (root.left != null) {
+            l = maxDepth(root.left);
+        }
+        return Math.max(r, l) + 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
