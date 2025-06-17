bb986e0f0ee60f376725434c9073116779c40496
vedgit872 <vedsmith18@gmail.com>
2025-06-17 23:54:44 +0530

    Day 49-Arrays and Bit Manipulation
    Here in Single number XOR with all numbers and the result will be the single number. In Count prime numbers we use Boolean array to keep track of multiples of of prime numbers.

---
+++ leetcode/math/SingleNumber.java
@@ -0,0 +1,19 @@
+package leetcode.math;
+
+/*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+ */
+
+public class SingleNumber {
+     public int singleNumber(int[] nums) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+           k=k^nums[i];
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
